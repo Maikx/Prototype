a2b54f36848e2181797a801d6b57823b820d9cd4
Maikx <mikeybee2000@gmail.com>
2021-11-22 16:30:26 +0100

    Player Can Run/Push Objects + Cleaned Script

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,28 +4,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool CanMove { get; private set; } = true;
+    private bool isRunning => canRun && Input.GetKey(sprintKey);
+    [HideInInspector]
     public CharacterController controller;
     private Vector3 direction;
-    public float speed = 8;
+
+    [Header("Function Options")]
+    [SerializeField] private bool canRun = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Movement Parameters")]
+    public float walkSpeed = 8;
+    public float runSpeed = 20;
     public float jumpForce = 10;
     public float gravity = -20;
+
+    [Header("Misc Parameters")]
     public Transform groundCheck;
     public LayerMask groundLayer;
 
     void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        direction.x = hInput * speed;
+        if(CanMove)
+        {
+            HandleMovementInput();
+        }
+    }
 
+    private void HandleMovementInput()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        direction.x = hInput * (isRunning ? runSpeed : walkSpeed);
         bool isGrounded = Physics.CheckSphere(groundCheck.position, 0.15f, groundLayer);
-        direction.y += gravity * Time.deltaTime;
-        if(isGrounded)
+        if (isGrounded)
         {
             if (Input.GetButtonDown("Jump"))
             {
                 direction.y = jumpForce;
             }
+            canRun = true;
+        }
+        else
+        {
+            canRun = false;
         }
         controller.Move(direction * Time.deltaTime);
+        direction.y += gravity * Time.deltaTime;
     }
 }
