a7d6c5a1fd65ac34f65ff506d4e21824905bff38
Maikx <mikeybee2000@gmail.com>
2022-01-09 17:35:27 +0100

    Companion checks if walls are nearby + interacts

--- Assets/Scripts/Companion/Companion.cs
+++ Assets/Scripts/Companion/Companion.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Companion : MonoBehaviour
 {
+    private bool isInteracting => canInteract && Input.GetKey(interactKey);
+
     public Camera cam;
     public GameObject player;
+
+    [Header("Function Options")]
+    [SerializeField] private bool canInteract = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode interactKey = KeyCode.E;
+
+    [Header("Parameters")]
     public float depth = 5.0f;
     private float currentBoundary;
     public float minBoundary = 5.0f;
@@ -13,6 +23,7 @@ public class Companion : MonoBehaviour
     public float scaleSpeed = 2;
     private bool resize;
     private Vector3 point;
+    public float rayDist;
 
     void Start()
     {
@@ -22,8 +33,12 @@ public class Companion : MonoBehaviour
     private void Update()
     {
         Boundary();
+        CheckWalls();
     }
 
+    /// <summary>
+    /// Makes companion move with cursor & specifies distance from bond object (player)
+    /// </summary>
     public void Boundary()
     {
         Vector3 mousePos = Input.mousePosition;
@@ -45,5 +60,45 @@ public class Companion : MonoBehaviour
             currentBoundary = Mathf.Lerp(currentBoundary, minBoundary, Time.deltaTime * scaleSpeed);
         else
             currentBoundary = Mathf.Lerp(currentBoundary, maxBoundary, Time.deltaTime * scaleSpeed);
+    }
+
+    /// <summary>
+    /// Checks if walls are nearby..
+    /// </summary>
+    public void CheckWalls()
+    {
+            //The Raycast is used to check if there is a rigidbody that the player can grab.
+            RaycastHit2D right = Physics2D.Raycast(gameObject.transform.position, Vector2.right * transform.localScale, rayDist);
+            RaycastHit2D left = Physics2D.Raycast(gameObject.transform.position, Vector2.left * transform.localScale, rayDist);
+            RaycastHit2D up = Physics2D.Raycast(gameObject.transform.position, Vector2.up * transform.localScale, rayDist);
+            RaycastHit2D down = Physics2D.Raycast(gameObject.transform.position, Vector2.down * transform.localScale, rayDist);
+
+        if (right.collider != null && right.collider.gameObject.layer == 8)
+        {
+            //Debug.Log("Wall Right!");
+        }
+        if (left.collider != null && left.collider.gameObject.layer == 8)
+        {
+            //Debug.Log("Wall Left!");
+        }
+        if (up.collider != null && up.collider.gameObject.layer == 8)
+        {
+            //Debug.Log("Wall Up!");
+        }
+        if (down.collider != null && down.collider.gameObject.layer == 8)
+        {
+            //Debug.Log("Wall Down!");
+        }
+
+    }
+
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //This will be used for interactable objects..
+        if(other.tag == "CompanionInteractableObject" && Input.GetKey(interactKey))
+        {
+
+        }
     }
 }
