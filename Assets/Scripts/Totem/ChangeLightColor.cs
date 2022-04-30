b42d7c843f003657abb66cd373aa8b209f364c7f
Tommiconlai <tommiconlai@gmail.com>
2022-04-30 18:35:23 +0200

    inseriti caratteri per incisioni totem

--- Assets/Scripts/Totem/ChangeLightColor.cs
+++ Assets/Scripts/Totem/ChangeLightColor.cs
@@ -8,12 +8,19 @@ public class ChangeLightColor : MonoBehaviour
     public GameObject spotlight;
     public Animator totemAnimator;
 
+    [Header("Material")]
+    public Material newMaterial;
+    public GameObject carattere1, carattere2;
+
     void OnTriggerEnter2D(Collider2D target)
     {
         if (target.tag == "Ephemeral")
         {
             totemAnimator.SetTrigger("TotemUp");
             spotlight.GetComponent<Light>().color = color;
+
+            carattere1.GetComponent<MeshRenderer>().material = newMaterial;
+            carattere2.GetComponent<MeshRenderer>().material = newMaterial;
         }
     }
 }
