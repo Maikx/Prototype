93c2d495d109968175079fdb79cab27dbd9d14a3
Tommiconlai <tommiconlai@gmail.com>
2022-05-03 11:26:58 +0200

    aggiunta texture pelo + fix animazione totem

--- Assets/Scripts/Totem/ChangeLightColor.cs
+++ Assets/Scripts/Totem/ChangeLightColor.cs
@@ -6,21 +6,27 @@ public class ChangeLightColor : MonoBehaviour
 {
     public Color color;
     public GameObject spotlight;
-    public Animator totemAnimator;
 
-    [Header("Material")]
-    public Material newMaterial;
-    public GameObject carattere1, carattere2;
+    [Header("Animators")]
+    public Animator totemAnimator;
+    //public Animator carattere1Animator;
+    //public Animator carattere6Animator;
+
+    //[Header("Material")]
+    //public Material newMaterial;
+    //public GameObject carattere1, carattere2;
 
     void OnTriggerEnter2D(Collider2D target)
     {
         if (target.tag == "Ephemeral")
         {
             totemAnimator.SetTrigger("TotemUp");
+            //carattere1Animator.SetTrigger("Carattere1Up");
+            //carattere6Animator.SetTrigger("Carattere6Up");
             spotlight.GetComponent<Light>().color = color;
 
-            carattere1.GetComponent<MeshRenderer>().material = newMaterial;
-            carattere2.GetComponent<MeshRenderer>().material = newMaterial;
+            //carattere1.GetComponent<MeshRenderer>().material = newMaterial;
+            //carattere2.GetComponent<MeshRenderer>().material = newMaterial;
         }
     }
 }
