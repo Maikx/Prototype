fe45a09a6c626e96a6ffba353707d86b12555253
Tommiconlai <tommiconlai@gmail.com>
2022-05-19 01:04:19 +0200

    Aggiunta possibilità di dare un nome al cane

--- Assets/Continue.cs
+++ Assets/Continue.cs
@@ -7,10 +7,21 @@ public class Continue : MonoBehaviour
 {
     public int levelToLoad;
 
+    void Start()
+    {
+        StartCoroutine(ForcedPlay());
+    }
+
     public void LoadNextScene(int levelIndex)
     {
         Cursor.visible = false;
         levelIndex = levelToLoad;
         SceneManager.LoadScene(levelToLoad);
+    }
+
+    IEnumerator ForcedPlay()
+    {
+        yield return new WaitForSeconds(24.0f);
+        LoadNextScene(levelToLoad);
     }
 }
