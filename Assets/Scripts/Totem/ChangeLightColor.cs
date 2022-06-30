5187ce18a93f092fd633aef6df94df268275055a
Tommiconlai <tommiconlai@gmail.com>
2022-06-30 12:35:50 +0200

    Aggiunta erba fix suoni

--- Assets/Scripts/Totem/ChangeLightColor.cs
+++ Assets/Scripts/Totem/ChangeLightColor.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class ChangeLightColor : MonoBehaviour
 {
     public Color color;
+    public GameObject totem, Light;
     
 
     public AudioSource TotemActive, RockLift;
@@ -15,20 +17,22 @@ public class ChangeLightColor : MonoBehaviour
     //public Animator carattere1Animator;
     //public Animator carattere6Animator;
 
-    //[Header("Material")]
-    //public Material newMaterial;
+    [Header("Material")]
+    public Material newMaterial;
     //public GameObject carattere1, carattere2;
 
     void OnTriggerEnter2D(Collider2D target)
     {
         if (target.tag == "Ephemeral")
         {
+            //Light.SetActive(true);
+            Light.GetComponent<Light2D>().intensity = Mathf.PingPong(10, 2);
             totemAnimator.SetTrigger("TotemUp");
             //carattere1Animator.SetTrigger("Carattere1Up");
             //carattere6Animator.SetTrigger("Carattere6Up");
             TotemActive.Play();
             StartCoroutine(WaitRockLift());
-            //carattere1.GetComponent<MeshRenderer>().material = newMaterial;
+            totem.GetComponent<MeshRenderer>().material = newMaterial;
             //carattere2.GetComponent<MeshRenderer>().material = newMaterial;
         }
     }
